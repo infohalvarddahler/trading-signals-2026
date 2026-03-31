24093e439c07c9c751e33f77284ba95e38a25e9e
infohalvarddahler <infohalvarddahler@gmail.com>
2026-03-31 15:26:28 +0200

    Update trading_signals.py

--- trading_signals.py
+++ trading_signals.py
@@ -18,7 +18,7 @@ import sys
 # ============================================================
 
 # Your tickers (add as many as you want)
-TICKERS = ["MU", "SMCI", "AAPL", "NVDA", "TSLA"]
+TICKERS = ["MU", "EXOD", "", "", ""]
 
 # Number of days to show in output
 ANALYSIS_DAYS = 10
